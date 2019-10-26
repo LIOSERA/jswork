3dc3f803480c316d11b9c0446ead5d0932f16c80
LIOSERA <1324834624@qq.com>
2019-10-26 10:14:28 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,34 @@
+function calculate(){
+    let rum = document.getElememntById('').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value = pi()
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i= ){
+        
+    }
+
+
+return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1){
+
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+    }
+    return
+}
+
+
+calculate()
