d91997a50d191afe297c174d7b0e48b89c66c4d9
LIOSERA <1324834624@qq.com>
2019-12-08 19:44:37 +0800

    work5

--- work6/pai.js
+++ work6/pai.js
@@ -1,5 +1,5 @@
 function calculate(){
-    let rum = document.getElememntById('').value
+    let rum = document.getElememntById('rum').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
         document.getElementById('').value = pi()
